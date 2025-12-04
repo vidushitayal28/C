fd3d73567202efbe88029f44bb7a9cd37340393d
vidushitayal28 <vidushi.tayal28@gmail.com>
2025-12-04 18:47:15 +0530

    Add files via upload

---
+++ kilometers_to_miles.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+float km,miles;
+    printf("Enter distance in km : \n");
+    scanf("%f",&km);
+
+    (miles = 0.621 * km);
+
+    printf("Same distance in miles is equal to : %.2f\n",miles);
+    
+}
---
+++ max 2 no.s.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main (){
+    int number;
+    printf("Enter an integer number\n");
+    scanf("%d",&number);
+
+    if(number<100)
+    printf("your number is smaller than 100\n");
+
+    else
+    printf("your number contains more than 2 digits\n");
+    
+
+}
+ 
