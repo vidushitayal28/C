67f5ef4416a1e8b52877f78c7182b21c00efbf47
vidushitayal28 <vidushi.tayal28@gmail.com>
2025-12-07 10:08:53 +0530

    Add files via upload

---
+++ student_grade.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+float average_marks;
+char grade;
+printf("Enter average marks of student = \n");
+scanf("%f",&average_marks);
+if (100 <average_marks =<95)
+printf("GRADE A+");
+    else if(95 < average_marks =< 90)
+    Printf("GRADE A");
+        else if(90 <average_marks =<85);
+        Printf("GRADE B+");
+            else if(85 <average_marks =<80);
+            Printf("GRADE B");
+else printf("GRADE C");            
