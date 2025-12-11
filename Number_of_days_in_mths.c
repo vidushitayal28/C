44c5d502d31d31c435ff6053a57e14f07a857738
vidushitayal28 <vidushi.tayal28@gmail.com>
2025-12-11 22:45:28 +0530

    Add files via upload

---
+++ Number_of_days_in_mths.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+int month;
+printf("Enter month number: ");
+scanf("%d",&month);
+switch (month){
+	case 1: //January
+	case 3: //March
+	case 5: //May
+	case 7: //July
+	case 8: //August
+	case 10: //October
+	case 12: //December 
+printf("31 days\n");
+	break;
+
+	case 4: //April 
+	case 6: //June
+	
+	case 9: //September
+		case 11: //November 
+	printf("30 days\n");
+	break;
+
+	case 2: //February 
+	printf("28 or 29 days\n");
+	break;
+
+	default:
+	printf("Invalid month number\n");
+	break;
+}
+	return 0;
+}
